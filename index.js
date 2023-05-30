c8dabb4caab5674b337211b3e4e1e08c2efe0712
FLASHPOINT <juwan473jeremiah@gmail.com>
2023-05-30 11:02:35 -0600

    initial project

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent= count
+    
+}
+
+function save() {
+    let v="[" + count + "] "
+    saveEl.textContent+= v
+    console.log(count)
+    countEl.textContent=0
+    count=0
+    
+}
