fe9aa162b312a416804891335416b06afa40a956
FLASHPOINT <juwan473jeremiah@gmail.com>
2023-05-31 09:35:55 -0600

    added total rep display

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+/*******************************************************************************\
+* AUTHOR: JUWAN JEREMIAH                                                        *
+* DATE: 05/31/2023                                                              *
+* ENVIRONMENT: VSCODE                                                           *
+* TITLE: PULL UP REP COUNTER                                                    *
+* DESCRIPTION: THIS PROGRAM INCREMENTS WITH THE PRESS OF THE "INCREMENT" BUTTON *
+*             AND SAVES EACH SET WITH THE USE OF THE "SAVE" BUTTON. THE TOTAL   *
+*             AMOUNT OF REPS IS ALSO DISPLAYED.                                 *
+********************************************************************************/
+
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-let count = 0
+let totalEl = document.getElementById("total-el")
+let count1 = 0
+let count2 = 0
 
 function increment() {
-    count += 1
-    countEl.textContent= count
-    
+    count1 += 1
+    count2 += 1
+    countEl.textContent= count1
+    totalEl.textContent= "TOTAL REPS: " + count2
 }
 
 function save() {
-    let v="[" + count + "] "
-    saveEl.textContent+= v
-    console.log(count)
+    saveEl.textContent+= "[" + count1 + "]"
     countEl.textContent=0
-    count=0
-    
+    count1=0
 }
